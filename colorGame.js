eb96bc7a6e578b7322723ab9a85d871b5f405c64
Praffulsingh9 <prafful.singh017@gmail.com>
2019-01-15 00:45:30 +0530

    GenerateRandomColors method added

--- colorGame.js
+++ colorGame.js
@@ -1,14 +1,8 @@
-var colors = [
-	"rgb(255 ,0 ,0)",
-	"rgb(255 ,255 ,0)",
-	"rgb(0 ,255 ,0)",
-	"rgb(0, 255 ,255)",
-	"rgb(0, 0, 255)",
-	"rgb(255 ,0 ,255)"
-];
+var colors = generateRandomColors(6);
+var pickedColor = pickColor();
 
 var squares = document.querySelectorAll(".square");
-var pickedColor = pickColor();
+var h1 = document.querySelector("h1");
 var colorDisplay = document.getElementById("colorDisplay");
 var messageDisplay = document.querySelector("#message");
 
@@ -23,7 +17,8 @@ for (var i = 0; i < squares.length; i++) {
 
 		if(clickedColor === pickedColor){
 			messageDisplay.textContent = "Correct";
-			changeColors(pickedColor);
+			changeColors(clickedColor);
+			h1.style.background = clickedColor;
 		} else {
 			this.style.background = "#232323";
 			messageDisplay.textContent = "Try Again";
@@ -42,4 +37,23 @@ function changeColors(color) {
 function pickColor() {
 	var random = Math.floor(Math.random() * colors.length);
 	return colors[random];
+}
+
+function generateRandomColors(num) {
+	var arr = [];
+
+	for (var i = 0 ; i < num; i++){
+		arr.push(randomColor());
+	}
+	return arr;
+}
+
+function randomColor() {
+	var r = Math.floor(Math.random() * 256);
+
+	var g = Math.floor(Math.random() * 256);
+
+	var b = Math.floor(Math.random() * 256);
+
+	return "rgb(" + r + ", " + g + ", " + b + ")";
 }
